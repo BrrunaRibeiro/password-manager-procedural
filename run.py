bac8294b0bfdc3e6f22dad5fe9853432908109a2
BrrunaRibeiro <brrunarib@gmail.com>
2023-10-31 10:08:50 +0100

    Innitial Commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from cryptography.fernet import Fernet
+
+print("Welcome to Password Manager\n")
+print("Please select one of the options below to continue...\n")
+print("1. View all stored accounts")
+print("2. Add new account")
+print("3. View an specific account")
+print("4. Update an account")
+print("5. Delete an account")
+print("6. Leave Password Manager")
+
+option_selected = input("Enter your option: ")
